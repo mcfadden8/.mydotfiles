24692c6074daec1335bd5fcb2e7dae3ce52fb3ed
Marty McFadden <mcfadden8@llnl.gov>
2019-05-28 11:06:51 -0700

    Added my own direcotory aliases

---
+++ zsh/omz_custom/plugins/my_directories/my_directories.plugin.zsh
@@ -0,0 +1,14 @@
+# Changing/making/removing directory
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias d='dirs -v | head -20'
+
+# List directory contents
+alias lsa='ls -lah'
+alias l='ls -SAF'
+alias ll='ls -lh'
+alias la='ls -lAh'
+alias lt='ls -lt'
+alias lrt='ls -lrt'
